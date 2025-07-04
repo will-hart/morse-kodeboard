69dba682eeb4b8068dec53cdef783ca31839522b
William Hart <391950+will-hart@users.noreply.github.com>
2025-07-05 09:04:51 +1000

    Implement morse code decoder
    This is still all untested :D
    
    Signed-off-by: William Hart <391950+will-hart@users.noreply.github.com>

--- src/debouncer.rs
+++ src/debouncer.rs
@@ -1,28 +1,45 @@
-use embassy_rp::gpio::{Input, Level};
-use embassy_time::{Duration, Timer};
+//! Contains code for firmware debouncing of inputs using 12 consecutive checks.
+//! The value `1` is considered "on".
 
-pub struct Debouncer<'a> {
-    input: Input<'a>,
-    debounce: Duration,
+/// A debounced input that checks `N` times whether a switch is "on" before
+/// returning that it is "on".
+#[derive(Default, Clone, Copy)]
+pub struct DebouncedInput<const N: u8>(u16);
+
+#[derive(Default)]
+/// Returned by the [DebouncedInput::debounce] function, to denote whether the
+/// input has changed and whether the input is currently on.
+pub struct DebounceResult {
+    /// Has the value changed in the most recent update?
+    pub is_changed: bool,
+    /// Is the value currently "on"? Note that "on" may be high or low depending
+    /// on the hardware configuration)
+    pub is_on: bool,
 }
 
-impl<'a> Debouncer<'a> {
-    pub fn new(input: Input<'a>, debounce: Duration) -> Self {
-        Self { input, debounce }
+impl<const N: u8> DebouncedInput<N> {
+    /// Returns a new instance of self, panicing if N >= 16
+    pub const fn new() -> Self {
+        if N >= 16 {
+            panic!("Attempted to debounce an input with N >= 16");
+        }
+
+        Self(0xe000)
     }
 
-    pub async fn debounce(&mut self) -> Level {
-        loop {
-            let l1 = self.input.get_level();
+    /// Debounces the given input taking the current value and returning `true`
+    /// if the input is on after debouncing. Note that "on" may be high or low
+    /// in hardware, but a boolean should be passed here which is `true` if the
+    /// input is currently on
+    pub fn debounce(&mut self, is_on: bool) -> DebounceResult {
+        let previous_value = self.0 == 0xf000;
 
-            self.input.wait_for_any_edge().await;
+        self.0 = (self.0 << 1) | if is_on { 0 } else { 1 } | 0xe000;
+        let is_on = self.0 == 0xf000;
 
-            Timer::after(self.debounce).await;
-
-            let l2 = self.input.get_level();
-            if l1 != l2 {
-                break l2;
-            }
+        DebounceResult {
+            is_changed: is_on != previous_value,
+            is_on,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use debouncer::DebounceResult;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_futures::join::join;
@@ -8,7 +9,10 @@ use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Input, Pull};
 use embassy_rp::peripherals::USB;
 use embassy_rp::usb::{Driver, InterruptHandler};
-use embassy_time::Duration;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::{Channel, Sender};
+use embassy_sync::mutex::Mutex;
+use embassy_time::{Duration, Instant, Ticker, Timer};
 use embassy_usb::class::hid::State;
 use usbd_hid::descriptor::KeyboardReport;
 use {defmt_rtt as _, panic_probe as _};
@@ -20,11 +24,67 @@ bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
 });
 
+type EventChannel = Channel<ThreadModeRawMutex, u8, 32>;
+type EventSender = Sender<'static, ThreadModeRawMutex, u8, 32>;
+static EVENT_CHANNEL: EventChannel = Channel::new();
+
+type ButtonType = Mutex<ThreadModeRawMutex, Option<Input<'static>>>;
+static MORSE_BUTTON: ButtonType = Mutex::new(None);
+
+#[embassy_executor::task]
+async fn generate_morse_code_characters(morse_btn: &'static ButtonType, sender: EventSender) {
+    let mut morse_decoder = morse_codec::decoder::Decoder::<16>::new()
+        .with_reference_short_ms(100)
+        .build();
+    let mut btn_debouncer = debouncer::DebouncedInput::<12>::new();
+    let mut ticker = Ticker::every(Duration::from_millis(2));
+    let mut last_change: Instant = Instant::now();
+
+    loop {
+        // debounce the input
+        let result = {
+            let btn_unlocked = morse_btn.lock().await;
+
+            if let Some(btn_ref) = btn_unlocked.as_ref() {
+                btn_debouncer.debounce(btn_ref.is_high())
+            } else {
+                DebounceResult::default()
+            }
+        };
+
+        // update the morse decoder
+        if result.is_changed {
+            // register the event with the morse decoder
+            let this_change = Instant::now();
+            let delta = last_change - this_change;
+            last_change = this_change;
+
+            morse_decoder.signal_event(delta.as_millis() as u16, result.is_on);
+
+            // check if we have a message to send on the keyboard
+            let len = morse_decoder.message.len();
+            if len > 0 {
+                let msg = morse_decoder.message.as_charray();
+                for &ch in msg.iter().take(len) {
+                    sender.send(ch).await;
+                }
+
+                morse_decoder.message.clear();
+            }
+        }
+
+        // only check inputs periodically
+        ticker.next().await;
+    }
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut morse_btn =
-        debouncer::Debouncer::new(Input::new(p.PIN_9, Pull::Up), Duration::from_millis(20));
+    let morse_btn = Input::new(p.PIN_9, Pull::Up);
+    {
+        *(MORSE_BUTTON.lock().await) = Some(morse_btn);
+    }
 
     // Create the driver, from the HAL and required buffers and handlers
     let driver = Driver::new(p.USB, Irqs);
@@ -46,38 +106,56 @@ async fn main(_spawner: Spawner) {
     let usb_fut = usb.run();
     let (reader, mut writer) = hid.split();
 
+    let event_receiver = EVENT_CHANNEL.receiver();
+
     // set up a listening / transmitting loop for the USB interface
     let in_fut = async {
         loop {
-            info!("Waiting for HIGH on pin 16");
-            morse_btn.debounce().await;
-            info!("HIGH DETECTED");
-            // Create a report with the A key pressed. (no shift modifier)
-            let report = KeyboardReport {
-                keycodes: [4, 0, 0, 0, 0, 0],
-                leds: 0,
-                modifier: 0,
-                reserved: 0,
-            };
-            // Send the report.
-            match writer.write_serialize(&report).await {
-                Ok(()) => {}
-                Err(e) => warn!("Failed to send report: {:?}", e),
-            };
-            morse_btn.debounce().await;
+            match event_receiver.try_receive() {
+                Ok(char) => {
+                    info!("Sending Key");
+                    // Create a report with the A key pressed. (no shift modifier)
+                    let report = KeyboardReport {
+                        keycodes: [char, 0, 0, 0, 0, 0],
+                        leds: 0,
+                        modifier: 0,
+                        reserved: 0,
+                    };
+                    // Send the report.
+                    match writer.write_serialize(&report).await {
+                        Ok(()) => {}
+                        Err(e) => warn!("Failed to send report: {:?}", e),
+                    };
+
+                    // delay 10ms before we release the key
+                    Timer::after(Duration::from_millis(10)).await;
+
+                    info!("Releasing Key");
+                    let report = KeyboardReport {
+                        keycodes: [0, 0, 0, 0, 0, 0],
+                        leds: 0,
+                        modifier: 0,
+                        reserved: 0,
+                    };
+                    // Send the report.
+                    match writer.write_serialize(&report).await {
+                        Ok(()) => {}
+                        Err(e) => warn!("Failed to send report: {:?}", e),
+                    };
+                }
+                Err(_err) => {
+                    // nop - we just move on
+                }
+            }
+
             info!("LOW DETECTED");
-            let report = KeyboardReport {
-                keycodes: [0, 0, 0, 0, 0, 0],
-                leds: 0,
-                modifier: 0,
-                reserved: 0,
-            };
-            match writer.write_serialize(&report).await {
-                Ok(()) => {}
-                Err(e) => warn!("Failed to send report: {:?}", e),
-            };
         }
     };
+
+    unwrap!(spawner.spawn(generate_morse_code_characters(
+        &MORSE_BUTTON,
+        EVENT_CHANNEL.sender()
+    )));
 
     let mut request_handler = usb::MyRequestHandler {};
     let out_fut = async {
